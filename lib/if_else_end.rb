d2ab25afb73c4c58debee6e802e0cc9e39e3b020
practical-kernel-3029 <nah1222@yahoo.com>
2020-07-23 21:50:35 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,9 @@
 # Write your solution here
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
 end
-puts "Code after if...end"
